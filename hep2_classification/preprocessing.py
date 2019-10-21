e84a19b89741b1a57aec6a3c97349fccd554d415
Jan Gołda <jan.golda@gmail.com>
2019-10-21 13:15:29 +0200

    Added tag removal Moved most of variables in pre-processing module to constants

--- hep2_classification/preprocessing.py
+++ hep2_classification/preprocessing.py
@@ -1,8 +1,45 @@
 """ Module containing all pre-processing related functions. """
-from typing import Tuple
+import os
+from typing import Tuple, Optional
 
 import numpy as np
 import cv2 as cv
+
+# constant default settings for pre-processing
+IMG_SIZE = (800, 600)
+TAG_TEMPLATE = cv.imread(os.path.join(os.path.dirname(__file__), 'tag-template-20.tif'))
+TAG_SEARCH_RANGE = (slice(70), slice(340))
+TAG_MIN_MATCH = 3 * 10**6
+TAG_CUTOFF = (slice(55, None), slice(None, None))
+DENOISE_STRENGTH = 2
+
+
+def _match_tag(img: np.ndarray,
+               search_range: Tuple[slice, slice] = TAG_SEARCH_RANGE,
+               tag_template: np.ndarray = TAG_TEMPLATE
+               ) -> np.ndarray:
+    """
+    Calculates match probability for tag in given image.
+
+    :param img: image to search in
+    :param search_range: selector used to restrict search region
+    :param tag_template: template to match
+    :return: array of probabilities returned by `cv2.matchTemplate`
+    """
+    return cv.matchTemplate(img[search_range], tag_template, cv.TM_CCOEFF)
+
+
+def _remove_tag(img: np.ndarray, min_match: float = TAG_MIN_MATCH) -> np.ndarray:
+    """
+    Removes tag (if it is present) by cutting of top part of the image.
+
+    :param img: image to remove tag from
+    :param min_match: minimum match value for which tag will be removed
+    :return: image with removed (or not) part containing tag
+    """
+    if _match_tag(img).max() > min_match:
+        return img[TAG_CUTOFF]
+    return img
 
 
 def _reduce_channels(img: np.ndarray) -> np.ndarray:
@@ -10,12 +47,12 @@ def _reduce_channels(img: np.ndarray) -> np.ndarray:
     return cv.cvtColor(img, code=cv.COLOR_BGR2GRAY)
 
 
-def _resize(img: np.ndarray, size: Tuple[int, int]) -> np.ndarray:
+def _resize(img: np.ndarray, size: Tuple[int, int] = IMG_SIZE) -> np.ndarray:
     """ Resizes given image to given size using cubic interpolation. """
     return cv.resize(img, dsize=size, interpolation=cv.INTER_CUBIC)
 
 
-def _denoise(img: np.ndarray, h: float) -> np.ndarray:
+def _denoise(img: np.ndarray, h: float = DENOISE_STRENGTH) -> np.ndarray:
     """ Removes noise from given image. Parameter `h` sets filter strength. """
     return cv.fastNlMeansDenoising(img, h=h)
 
