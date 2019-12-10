afdf5d0f845bb88188c55da1d18ff18471649e1d
Jan Gołda <jan.golda@gmail.com>
2019-12-10 15:36:06 +0100

    Added simple function that performs process

--- hep2_classification/__init__.py
+++ hep2_classification/__init__.py
@@ -1,3 +1,5 @@
 """ Main project module """
 from .preprocessing import preprocess
 from .segmentation import segmentate, SegmentationResult, Segment
+from .classification import BaseClassifier, RandomClassifier
+from .pipeline import classify_image
