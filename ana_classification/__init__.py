8166ced46ed36ee1c9b0befd96227ee031d8eb94
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-19 22:33:46 +0800

    Delete ana_classification directory

--- ana_classification/__init__.py
+++
@@ -1,5 +0,0 @@
-""" Main project module """
-from .preprocessing import preprocess
-from .segmentation import segmentate, SegmentationResult, Segment
-from .classification import BaseCellClassifier, RandomCellClassifier, ConvNetCellClassifier, NegClassifier
-from .pipeline import classify_image
